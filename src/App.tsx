beac394be931991a5fd69688000e7ef60a2576fa
dmitriyzolotukhin653 <dmitriyzolotukhin653@gmail.com>
2022-06-10 13:47:52 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default App;
